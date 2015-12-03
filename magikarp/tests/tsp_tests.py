3d02fe4adb3ab3ceeb64dfb638c3a49568904523
lmc2179 <louiscialdella@gmail.com>
2015-12-02 23:28:14 -0500

    exhaustive solution for tsp

--- magikarp/tests/tsp_tests.py
+++ magikarp/tests/tsp_tests.py
@@ -1,6 +1,7 @@
 import unittest
-from magikarp.tsp import TravellingSalespersonProblem
+from magikarp.tsp import TravellingSalespersonProblem, ExhaustiveTSPSolver
 import math
+from numpy import argmin
 
 class MetricTSProblemTest(unittest.TestCase):
     def test_accessors(self):
@@ -13,3 +14,20 @@ class MetricTSProblemTest(unittest.TestCase):
         p = TravellingSalespersonProblem(points)
         self.assertEqual(p.evaluate_solution([0, 1, 2, 3]), 4)
         self.assertEqual(p.evaluate_solution([0, 2, 1, 3]), 2 + 2*math.sqrt(2))
+
+class ExhaustiveTSPSolverTest(unittest.TestCase):
+    def test_square(self):
+        points = [(0, 0), (1, 0), (1, 1), (0, 1)]
+        p = TravellingSalespersonProblem(points)
+        solution = ExhaustiveTSPSolver().solve(p)
+        self._assert_cyclic_equals(solution, [0, 1, 2, 3])
+
+    def _assert_cyclic_equals(self, l1, l2):
+        l1_canonical = self._get_canonical_cyclic_form(l1)
+        l2_canonical = self._get_canonical_cyclic_form(l2)
+        self.assertEqual(l1_canonical, l2_canonical)
+
+    def _get_canonical_cyclic_form(self, l):
+        l = list(l)
+        new_zero = argmin(l)
+        return l[new_zero:] + l[:new_zero]
--- magikarp/tsp.py
+++ magikarp/tsp.py
@@ -1,4 +1,5 @@
 import math
+import itertools
 
 class TravellingSalespersonProblem(object):
     def __init__(self, points):
@@ -14,3 +15,24 @@ class TravellingSalespersonProblem(object):
         circuit_index_pairs = [(p_i, p_i_next) for p_i, p_i_next in zip(path_indices[:-1], path_indices[1:])] + [(path_indices[-1], path_indices[0])]
         circuit_point_pairs = [(self.points[i1], self.points[i2]) for i1, i2 in circuit_index_pairs]
         return sum([self._distance(p1, p2) for p1, p2 in circuit_point_pairs])
+
+class ExhaustiveTSPSolver(object):
+    def solve(self, problem):
+        potential_solutions = self._get_potential_solutions(problem)
+        best = self._get_best_solution(potential_solutions, problem)
+        return best
+
+    def _get_potential_solutions(self, problem):
+        index_size = len(problem.get_points())
+        all_indices = list(range(index_size))
+        return itertools.permutations(all_indices)
+
+    def _get_best_solution(self, potential_solutions, problem):
+        shortest_so_far = float('inf')
+        best_so_far = None
+        for sol in potential_solutions:
+            solution_cost = problem.evaluate_solution(sol)
+            if solution_cost < shortest_so_far:
+                shortest_so_far = solution_cost
+                best_so_far = sol
+        return best_so_far
