35f365e91455c1917caabe8794b4f7c37dfecc29
lmc2179 <louiscialdella@gmail.com>
2015-12-02 23:14:38 -0500

    refactored evaluation_solution

--- magikarp/tsp.py
+++ magikarp/tsp.py
@@ -11,7 +11,6 @@ class TravellingSalespersonProblem(object):
         return math.sqrt(sum([(x1-x2)**2 for x1, x2 in zip(p1, p2)]))
 
     def evaluate_solution(self, path_indices):
-        # TODO oh my god this is a disaster
-        return sum([self._distance(self.points[p_i], self.points[p_i_next])
-                    for p_i, p_i_next in zip(path_indices[:-1], path_indices[1:])]) + self._distance(self.points[path_indices[-1]],
-                                                                                                     self.points[path_indices[0]])
+        circuit_index_pairs = [(p_i, p_i_next) for p_i, p_i_next in zip(path_indices[:-1], path_indices[1:])] + [(path_indices[-1], path_indices[0])]
+        circuit_point_pairs = [(self.points[i1], self.points[i2]) for i1, i2 in circuit_index_pairs]
+        return sum([self._distance(p1, p2) for p1, p2 in circuit_point_pairs])
