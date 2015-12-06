1ed709fe25db80a088f0c713ef49115cf7a2bd04
lmc2179 <louiscialdella@gmail.com>
2015-12-06 13:46:47 -0500

    added exhaustive solution for partition problem

--- magikarp/partition.py
+++ magikarp/partition.py
@@ -1,4 +1,5 @@
-from magikarp.abstract import AbstractProblem
+import itertools
+from magikarp.abstract import AbstractProblem, AbstractExhaustiveSolver
 
 class PartitionProblem(AbstractProblem):
     "Represents an instance of the partition difference optimization problem."
@@ -10,3 +11,21 @@ class PartitionProblem(AbstractProblem):
         p1_sum = sum([self.array[i] for i in p1_indices])
         p2_sum = sum([self.array[i] for i in p2_indices])
         return abs(p1_sum - p2_sum)
+
+    def get_array(self):
+        return self.array
+
+class ExhaustivePartitionSolver(AbstractExhaustiveSolver):
+    def _candidate_solution_is_better(self, candidate_solution_value, best_solution_value):
+        return  candidate_solution_value < best_solution_value
+
+    def _get_potential_solutions(self, problem):
+        index_list = range(len(problem.get_array()))
+        index_set = set(index_list)
+        for first_partition_size in range(1, int(len(index_set) / 2) + 1):
+            for first_partition in itertools.combinations(index_list, first_partition_size):
+                second_partition = list(index_set - set(first_partition))
+                yield first_partition, second_partition
+
+    def _get_worst_possible_solution_value(self):
+        return float('inf')
--- magikarp/tests/partition_tests.py
+++ magikarp/tests/partition_tests.py
@@ -8,5 +8,15 @@ class PartitionProblemTest(unittest.TestCase):
         self.assertEqual(p.evaluate_solution(((0,), (2, 1))), 0)
         self.assertEqual(p.evaluate_solution(((1,), (2,0))), 4)
 
+class ExhaustiveSolverTest(unittest.TestCase):
+    def test_optimal_solution(self):
+        p = partition.PartitionProblem([1, -1, 2])
+        sol = partition.ExhaustivePartitionSolver().solve(p)
+        p1, p2 = sol
+        self.assertNotEqual(p1, p2)
+        best_partitioning = [{0}, {1, 2}]
+        assert set(p1) in best_partitioning
+        assert set(p2) in best_partitioning
+
 if __name__ == '__main__':
     unittest.main()
