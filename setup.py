c3fb78b7b0d55009277e901b7ed92597a9b50d9d
lmc2179 <louiscialdella@gmail.com>
2015-12-07 21:31:26 -0500

    added setup.py, updated travis config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='magikarp',
+      version='0.1',
+      description='Exact and approximate solutions to NP-Hard problems',
+      author='Louis Cialdella',
+      author_email='louiscialdella@gmail.com',
+      url='https://github.com/lmc2179/magikarp',
+      packages=['magikarp'],
+     )
