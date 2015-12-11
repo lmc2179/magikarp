cc6f98b6a6d5d9308195444b21a72cc444fa6fd5
lmc2179 <louiscialdella@gmail.com>
2015-12-10 21:59:57 -0500

    first cut of simulated annealing

--- magikarp/simulated_annealing.py
+++ magikarp/simulated_annealing.py
@@ -1,17 +1,29 @@
 import math
 import random
 
+from magikarp.abstract import AbstractSolver, MinMaxEnum
 
-class AbstractSimulatedAnnealing(object):
-    def __init__(self, cooling_constant):
-        self.cooling_constant = cooling_constant
 
-    def get_approximate_solution(self, initial_point, no_iterations):
+class AbstractSimulatedAnnealing(AbstractSolver):
+    def __init__(self, problem):
+        super(AbstractSimulatedAnnealing, self).__init__(problem)
+        self.EVALUATION_COEFF = self._get_evaluation_coeff()
+
+    def _get_evaluation_coeff(self):
+        MIN_MAX_TYPE = self.problem.MIN_MAX_TYPE
+        if MIN_MAX_TYPE == MinMaxEnum.MIN:
+            return 1.0 # No adjustment needed here, this is already a minimization problem
+        elif MIN_MAX_TYPE == MinMaxEnum.MAX:
+            return 1.0
+        raise Exception('Unrecognized MIN_MAX_TYPE for class {0}: {1}'.format(self.problem.__class__,
+                                                                              self.problem.MIN_MAX_TYPE))
+
+    def solve(self, initial_point, no_iterations, cooling_constant):
         best_score = float('inf')
         best_point = None
         current_point = initial_point
-        for i in range(no_iterations):
-            temp = self._get_temperature(i)
+        for i in range(no_iterations): #TODO: Uncle bob this
+            temp = self._get_temperature(i, cooling_constant)
             candidate_point = self._get_neighbor(current_point)
             acceptance_prob = self._get_acceptance_likelihood(current_point, candidate_point, temp)
             if acceptance_prob > random.random():
@@ -22,10 +34,10 @@ class AbstractSimulatedAnnealing(object):
                     best_point = current_point
         return best_point
 
-    def _get_temperature(self, iteration):
-        if iteration + 1 == 1:
-            return self.cooling_constant
-        return self.cooling_constant / math.log(iteration+1)
+    def _get_temperature(self, iteration, cooling_constant):
+        if iteration == 0:
+            return cooling_constant
+        return cooling_constant / math.log(iteration+1)
 
     def _get_neighbor(self, current_point):
         raise NotImplementedError
@@ -39,4 +51,4 @@ class AbstractSimulatedAnnealing(object):
             return math.exp(-(candidate_value - current_value) / temp)
 
     def _evaluate_point(self, p):
-        raise NotImplementedError # This is the function to minimize
+        return self.EVALUATION_COEFF * self.problem.evaluate_solution(p)
