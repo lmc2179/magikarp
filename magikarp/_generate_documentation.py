65d7d0544208931980d7a07789b3f781f06f88f4
lmc2179 <louiscialdella@gmail.com>
2015-12-12 19:20:59 -0800

    Added solver and problem class search

--- magikarp/_generate_documentation.py
+++ magikarp/_generate_documentation.py
@@ -1,7 +1,18 @@
+import inspect
+
 from magikarp import tsp, knapsack, partition
+from magikarp.abstract import AbstractSolver, AbstractProblem
+
 
 def generate_documentation(target_module):
     print('Generating documentation for {0}...'.format(target_module))
+    for name, obj in inspect.getmembers(target_module):
+        if inspect.isclass(obj):
+            if issubclass(obj, AbstractSolver):
+                print('Got Solver class {0}'.format(obj))
+            if issubclass(obj, AbstractProblem):
+                print('Got Problem class {0}'.format(obj))
+
 
 if __name__ == '__main__':
     target_modules = [tsp,
