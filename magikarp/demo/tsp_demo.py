dba8c319ce88c09f1cb4c5ed145d5eb6503bbbcb
lmc2179 <louiscialdella@gmail.com>
2015-12-20 12:22:37 -0800

    Added first version of tsp demo

---
+++ magikarp/demo/tsp_demo.py
@@ -0,0 +1,19 @@
+import random
+
+from matplotlib import pyplot as plt
+
+from magikarp import tsp, simulated_annealing
+
+NUM_CITIES = 15
+city_x = [random.random() for _ in range(NUM_CITIES)]
+city_y = [random.random() for _ in range(NUM_CITIES)]
+city_tuples = list(zip(city_x, city_y))
+
+p = tsp.TravellingSalespersonProblem(city_tuples)
+sa_tour = simulated_annealing.SimulatedAnnealingSolver(p, tsp.TSP2OptNeighborStrategy()).solve(list(range(0,NUM_CITIES)), 50000, 1000.0) + [0]
+print(sa_tour)
+sa_tour_x, sa_tour_y = [city_x[i] for i in sa_tour], [city_y[i] for i in sa_tour]
+
+plt.plot(city_x, city_y, marker='o', linewidth=0.0)
+plt.plot(sa_tour_x, sa_tour_y, color='r')
+plt.show()
