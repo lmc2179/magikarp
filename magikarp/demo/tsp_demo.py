5664abbcb3433794ddf9580dd5990ade8e7c6fa3
lmc2179 <louiscialdella@gmail.com>
2015-12-22 19:34:48 -0800

    Added hill climbing to demo

--- magikarp/demo/tsp_demo.py
+++ magikarp/demo/tsp_demo.py
@@ -4,16 +4,25 @@ from matplotlib import pyplot as plt
 
 from magikarp import tsp, point_search
 
-NUM_CITIES = 15
+def draw_tour(tour, city_x, city_y, color, fig_no):
+    plt.figure(fig_no)
+    plt.plot(city_x, city_y, marker='o', linewidth=0.0)
+    tour_x, tour_y = [city_x[i] for i in tour], [city_y[i] for i in tour]
+    plt.plot(tour_x, tour_y, color=color)
+
+NUM_CITIES = 50
 city_x = [random.random() for _ in range(NUM_CITIES)]
 city_y = [random.random() for _ in range(NUM_CITIES)]
 city_tuples = list(zip(city_x, city_y))
 
 p = tsp.TravellingSalespersonProblem(city_tuples)
-sa_tour = point_search.SimulatedAnnealingSolver(p, tsp.TSP2OptNeighborStrategy()).solve(list(range(0, NUM_CITIES)), 100000, 50.0) + [0]
-print(sa_tour)
-sa_tour_x, sa_tour_y = [city_x[i] for i in sa_tour], [city_y[i] for i in sa_tour]
 
-plt.plot(city_x, city_y, marker='o', linewidth=0.0)
-plt.plot(sa_tour_x, sa_tour_y, color='r')
+sa_tour = point_search.SimulatedAnnealingSolver(p, tsp.TSP2OptNeighborStrategy()).solve(list(range(0, NUM_CITIES)), 300000, 1.0)
+print("SA Tour: ", p.evaluate_solution(sa_tour))
+draw_tour(sa_tour + [0], city_x, city_y, 'r', 0)
+
+hc_tour = point_search.HillClimbingSolver(p, tsp.TSP2OptNeighborStrategy()).solve(list(range(0, NUM_CITIES)), 300000)
+print("HC Tour: ", p.evaluate_solution(hc_tour))
+draw_tour(hc_tour + [0], city_x, city_y, 'g', 1)
+
 plt.show()
